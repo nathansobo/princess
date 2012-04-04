2dd882d41d2b665038ce945fa8baa617eeaca22b
Nathan Sobo <nathansobo@gmail.com>
2012-04-03 20:34:43 -0600

    Add spec/assets/javascripts to the asset pipeline in initializer

--- lib/princess/engine.rb
+++ lib/princess/engine.rb
@@ -3,5 +3,9 @@ require 'rails'
 module Princess
   class Engine < Rails::Engine
     paths["vendor/assets"] = ["vendor/jasmine/lib", "vendor/jasmine/images"]
+
+    initializer "enable assets" do |app|
+      app.config.assets.paths << Rails.root.join('spec', 'assets', 'javascripts')
+    end
   end
 end
